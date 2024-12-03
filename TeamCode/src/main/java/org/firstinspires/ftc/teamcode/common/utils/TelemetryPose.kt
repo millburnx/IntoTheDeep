5542304cedfbbe10dcb96e97fedd8b1530b0746c
Ash <68016221+AshOnDiscord@users.noreply.github.com>
2024-12-03 13:37:04 +0000

    Full Purge

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/utils/TelemetryPose.kt
+++
@@ -1,5 +0,0 @@
-package org.firstinspires.ftc.teamcode.common.utils
-
-import com.arcrobotics.ftclib.geometry.Pose2d
-
-data class TelemetryPose(val pose: Pose2d, val color: String, val size: Double? = null)
