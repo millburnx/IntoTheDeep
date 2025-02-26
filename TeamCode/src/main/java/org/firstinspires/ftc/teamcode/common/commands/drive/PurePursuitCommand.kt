b1886dae915f5438b18092503c2e0f3f32039adf
AshOnDiscord <68016221+AshOnDiscord@users.noreply.github.com>
2025-02-26 02:43:36 -0500

    Traditional Heading PurePursuit w/ backwards support

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commands/drive/PurePursuitCommand.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commands/drive/PurePursuitCommand.kt
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.common.commands.drive
 import com.acmerobotics.dashboard.config.Config
 import com.arcrobotics.ftclib.command.CommandBase
 import com.millburnx.purepursuit.PurePursuit
+import com.millburnx.purepursuit.Util.getAngleDiff
 import com.millburnx.utils.Vec2d
 import org.firstinspires.ftc.teamcode.common.utils.Pose2d
 import org.firstinspires.ftc.teamcode.opmodes.auton.AutonRobot
@@ -13,7 +14,11 @@ class PurePursuitCommand(
     val targetHeading: Double,
     val path: List<Vec2d>,
     val lookahead: ClosedRange<Double> = 8.0..16.0,
+    val headingLock: Boolean = true, // true = heading-less pure pursuit, false = traditional pure pursuit
     val exitOnStuck: Boolean = false, // useful for if you're ramming into a wall
+    val multiF: Double = Companion.multiF,
+    val multiH: Double = Companion.multiH,
+    val backwards: Boolean = false, // only for if headingLock = false
 ) : CommandBase() {
     val purePursuit = PurePursuit(path, lookahead, PIDSettings.tolerance)
     val endingState = false
@@ -26,15 +31,41 @@ class PurePursuitCommand(
 
     override fun execute() {
         val pose = robot.drive.pose
-        val results = purePursuit.calc(pose.position, virtualHeading, robot.deltaTime.deltaTime)
+        val results =
+            purePursuit.calc(
+                pose.position,
+                if (headingLock) virtualHeading else pose.heading,
+                robot.deltaTime.deltaTime,
+            )
         robot.telemetry.addData("lookahead", results.lookahead)
-        robot.telemetry.addData("virtualHeading", virtualHeading)
         robot.telemetry.addData("target", results.target)
 
-        virtualHeading = Math.toDegrees(pose.position.angleTo(results.target))
+        if (headingLock) {
+            robot.telemetry.addData("virtualHeading", virtualHeading)
 
-        robot.pidManager.isOn = true
-        robot.pidManager.target = Pose2d(results.target, targetHeading)
+            virtualHeading = Math.toDegrees(pose.position.angleTo(results.target))
+
+            robot.pidManager.isOn = true
+            robot.pidManager.target = Pose2d(results.target, targetHeading)
+        } else {
+            val endDistance = pose.distanceTo(path.last())
+            if (endDistance < lookahead.start) {
+                robot.pidManager.isOn = true
+                robot.pidManager.target = Pose2d(path.last(), targetHeading)
+            } else {
+                val powerF = pose.distanceTo(results.target) * if (backwards) -1.0 else 1.0
+                val angleDiff =
+                    if (backwards) {
+                        getAngleDiff(pose.position to Math.toRadians(pose.degrees + 180), results.target)
+                    } else {
+                        getAngleDiff(pose.position to pose.radians, results.target)
+                    }
+                val powerH = Math.toDegrees(angleDiff)
+
+                robot.pidManager.isOn = false
+                robot.drive.robotCentric(powerF * multiF, 0.0, -powerH * multiH)
+            }
+        }
 
         val packet = robot.telemetryManager.currentPacket
         PurePursuit.render(results, packet, pose.position, false)
@@ -48,9 +79,9 @@ class PurePursuitCommand(
 
     companion object {
         @JvmField
-        var virtualHeadingSpeed = 1.0
+        var multiF = 1.0
 
         @JvmField
-        var maxVirtualHeadingSpeed = 90.0
+        var multiH = 1.0
     }
 }
